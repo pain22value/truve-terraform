ae75fbb16c4b023c392110de5ebacd4944b846c9
kiyun-kim <kiyn2403@gmail.com>
2026-03-25 00:14:54 +0900

    fix: 문법 수정

--- envs/dev/platform/argocd-ingress.tf
+++ envs/dev/platform/argocd-ingress.tf
@@ -9,13 +9,13 @@ resource "kubernetes_ingress_v1" "argocd" {
       "alb.ingress.kubernetes.io/target-type"        = "ip"
       "alb.ingress.kubernetes.io/listen-ports"       = "[{\"HTTP\":80}, {\"HTTPS\":443}]"
       "alb.ingress.kubernetes.io/ssl-redirect"       = "443"
-      "alb.ingress.kubernetes.io/certificate-arn"    = locals.argocd_acm_certificate_arn
+      "alb.ingress.kubernetes.io/certificate-arn"    = "${locals.argocd_acm_certificate_arn}"
       "alb.ingress.kubernetes.io/backend-protocol"   = "HTTPS"
       "alb.ingress.kubernetes.io/healthcheck-path"   = "/healthz"
       "alb.ingress.kubernetes.io/healthcheck-port"   = "traffic-port"
       "alb.ingress.kubernetes.io/success-codes"      = "200-399"
-      "alb.ingress.kubernetes.io/load-balancer-name" = locals.argocd_acm_certificate_arn
-      "alb.ingress.kubernetes.io/group.name"         = locals.argocd_alb_name
+      "alb.ingress.kubernetes.io/load-balancer-name" = "${locals.argocd_acm_certificate_arn}"
+      "alb.ingress.kubernetes.io/group.name"         = "${locals.argocd_alb_name}"
       "alb.ingress.kubernetes.io/group.order"        = "10"
     }
   }
