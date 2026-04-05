3c06ab2aa9b46d4a3ad42edcbc50f3ce900b8714
kiyun-kim <kiyn2403@gmail.com>
2026-04-06 07:36:48 +0900

    refactor(platform): ArgoCD ALB 제거

--- envs/dev/platform/locals.tf
+++ envs/dev/platform/locals.tf
@@ -2,10 +2,6 @@ locals {
   project_name = "truve"
   environment  = "dev"
 
-  argocd_acm_certificate_arn = "arn:aws:acm:ap-northeast-2:599476212908:certificate/c777a244-2261-47b1-aea0-0f68c9fb5249"
-  argocd_alb_name            = "argocd-alb"
-  argocd_host                = "argocd.truve.site"
-
   common_tags = {
     Project     = local.project_name
     Environment = local.environment
