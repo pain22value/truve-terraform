32baa3a7ce2dd5fd1e255cd62eab664ae4809e68
kiyun-kim <kiyn2403@gmail.com>
2026-04-08 10:48:55 +0900

    feat(argocd): argocd image updater 추가 및 스케줄링 설정

--- envs/dev/platform/argocd.tf
+++ envs/dev/platform/argocd.tf
@@ -47,3 +47,38 @@ resource "helm_release" "argocd" {
 
   depends_on = [kubernetes_namespace_v1.argocd]
 }
+
+resource "helm_release" "argocd_image_updater" {
+  name             = "argocd-image-updater"
+  namespace        = kubernetes_namespace_v1.argocd.metadata[0].name
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argocd-image-updater"
+  version          = "1.0.5"
+  create_namespace = false
+
+  timeout         = 600
+  atomic          = true
+  cleanup_on_fail = true
+
+  values = [
+    yamlencode({
+      nodeSelector = {
+        workload = "system"
+      }
+
+      tolerations = [
+        {
+          key      = "workload"
+          operator = "Equal"
+          value    = "system"
+          effect   = "NoSchedule"
+        }
+      ]
+    })
+  ]
+
+  depends_on = [
+    kubernetes_namespace_v1.argocd,
+    helm_release.argocd
+  ]
+}
